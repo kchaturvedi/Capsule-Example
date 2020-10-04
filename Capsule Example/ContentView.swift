a567a57562e289271f37427f1e49c7ba372894f9
Kartik Chaturvedi <kartikc@icloud.com>
2020-10-04 19:14:51 -0400

    Init

--- Capsule Example/ContentView.swift
+++ Capsule Example/ContentView.swift
@@ -9,13 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Capsule()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Capsule: View {
+    @State var selected: Bool = false
+    
+    var body: some View {
+        ZStack {
+            HStack {
+                if selected { Spacer() }
+                
+                Text(selected ? "Select" : "Delete")
+                
+                if !selected { Spacer() }
+            }
+            
+            HStack {
+                if !selected { Spacer() }
+                
+                Image(systemName: selected ? "checkmark" : "xmark")
+                    .padding(6)
+                    .background(Color.black.opacity(0.3))
+                    .cornerRadius(50)
+                    .rotationEffect(selected ? .zero : Angle(degrees: 270))
+                
+                if selected { Spacer() }
+            }
+        }
+        .animation(Animation.spring(response: 1, dampingFraction: 0.6, blendDuration: 0.1))
+        .foregroundColor(.white)
+        .frame(maxWidth: 100)
+        .padding(5)
+        .padding(.horizontal, 6)
+        .background(selected ? Color.green : Color.red)
+        .cornerRadius(50)
+        .onTapGesture {
+            selected.toggle()
+        }
     }
 }
