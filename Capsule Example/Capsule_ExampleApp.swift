91686371b4212c2e89e2550b174941dd407bf747
Kartik Chaturvedi <kartikc@icloud.com>
2020-10-04 19:12:47 -0400

    Initial Commit

---
+++ Capsule Example/Capsule_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Capsule_ExampleApp.swift
+//  Capsule Example
+//
+//  Created by Kartik Chaturvedi on 10/4/20.
+//
+
+import SwiftUI
+
+@main
+struct Capsule_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Capsule Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Capsule Example
+//
+//  Created by Kartik Chaturvedi on 10/4/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
